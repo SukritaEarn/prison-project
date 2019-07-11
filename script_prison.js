07dff7731f59092dd61857948112f8d4b48f419a
OreganoTea <44581946+OreganoTea@users.noreply.github.com>
2019-07-11 13:42:55 +0700

    update script 13.42

--- script_prison.js
+++ script_prison.js
@@ -5,18 +5,18 @@ let buzzer_status = ''
 let light_status = 'off'
 let light_button_status = ''
 const check_status = () =>{fetch(baseURL + '/data/pepsitrophy')
-                            .then((res) => res.json()).then((data)=>{
-                             console.log(data['door'])   
+                            .then((res) => res.json())
+                            .then((data)=>{
+                                door_status = data['door']
+                                light_status = data['light']
+                                buzzer_status = data['buzzer']
+                                console.log(door_status)
+                                if (door_status == 'off') door_button_status = 'on'
+                                else door_button_status = 'off'
+                                if (light_status == 'off') light_button_status = 'on'
+                                else light_button_status = 'off'   
                             })
-    //console.log(data['door'])
-    /*door_status = data['door']
-    light_status = data['light']
-    buzzer_status = data['buzzer']
-    if (door_status == 'off') door_button_status = 'on'
-    else door_button_status = 'off'
-    if (light_status == 'off') light_button_status = 'on'
-    else light_button_status = 'off'*/
-    
+                            
 }
 
 //setInterval(check_status,1000)
