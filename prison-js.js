891816cddf0ba9135efe682a0cb9bf5593a2689b
SukritaEarn <sukrita.ki@ku.th>
2019-07-11 14:38:57 +0700

    fix

--- prison-js.js
+++ prison-js.js
@@ -1,9 +1,14 @@
-let power = ["OFF", "ON"]
-let i = 0
-function changePower() {
-    document.getElementById("power_buzzer").innerText = n[i]
-    i++
-    if (i === n.length){
-        i = 0
-    }
+// let power = ["OFF", "ON"]
+// let i = 0
+// function changePower() {
+//     document.getElementById("power_buzzer").innerText = n[i]
+//     i++
+//     if (i === n.length){
+//         i = 0
+//     }
+// }
+
+function changeName() {
+    const inputValue = document.getElementById("power_buzzer").value;
+    document.getElementById("power_buzzer").innerText = "ON";
 }
